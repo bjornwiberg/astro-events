4a6fa8f7e1ff566f22860122381d4e6b8d8f712f
Björn Wiberg <b@bjrn.nu>
2023-02-19 19:10:00 +0100

    feat: new favicon and remove public folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,7 +44,10 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Astrological Events</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🪐</text></svg>"
+        />
       </Head>
 
       <main>
