565da537e56f2354e717f1b3d430a91e4182da7a
Björn Wiberg <b@bjrn.nu>
2023-02-20 12:20:30 +0100

    fix: use T in time fields for older browsers

--- data/events.ts
+++ data/events.ts
@@ -3,269 +3,269 @@ import { EventBaseType, EventType } from "../types/events";
 const equinox2023: EventBaseType[] = [
   {
     type: EventType.EQUINOX,
-    startDate: new Date("2023-03-20 22:24:00+01:00"),
+    startDate: new Date("2023-03-20T22:24:00+01:00"),
   },
   {
     type: EventType.EQUINOX,
-    startDate: new Date("2023-09-23 08:50:00+01:00"),
+    startDate: new Date("2023-09-23T08:50:00+01:00"),
   },
 ];
 const solstice2023: EventBaseType[] = [
   {
     type: EventType.SOLSTICE,
-    startDate: new Date("2023-06-21 16:58:00+01:00"),
+    startDate: new Date("2023-06-21T16:58:00+01:00"),
   },
   {
     type: EventType.SOLSTICE,
-    startDate: new Date("2023-12-22 04:27:00+01:00"),
+    startDate: new Date("2023-12-22T04:27:00+01:00"),
   },
 ];
 const hiatusSolar2023: EventBaseType[] = [
   {
     type: EventType.HIATUS_SOLAR,
-    startDate: new Date("2023-08-27 21:41:00+01:00"),
+    startDate: new Date("2023-08-27T21:41:00+01:00"),
   },
 ];
 const tripuraSundari2023: EventBaseType[] = [
   {
     type: EventType.TRIPURA_SUNDARI_PEAK,
-    startDate: new Date("2023-01-04 15:47+01:00"),
+    startDate: new Date("2023-01-04T15:47+01:00"),
   },
   {
     type: EventType.TRIPURA_SUNDARI_PEAK,
-    startDate: new Date("2023-02-03 10:41+01:00"),
+    startDate: new Date("2023-02-03T10:41+01:00"),
   },
   {
     type: EventType.TRIPURA_SUNDARI_PEAK,
-    startDate: new Date("2023-03-05 05:52+01:00"),
+    startDate: new Date("2023-03-05T05:52+01:00"),
   },
   {
     type: EventType.TRIPURA_SUNDARI_PEAK,
-    startDate: new Date("2023-04-04 00:57+01:00"),
+    startDate: new Date("2023-04-04T00:57+01:00"),
   },
   {
     type: EventType.TRIPURA_SUNDARI_PEAK,
-    startDate: new Date("2023-05-03 16:52+01:00"),
+    startDate: new Date("2023-05-03T16:52+01:00"),
   },
   {
     type: EventType.TRIPURA_SUNDARI_PEAK,
-    startDate: new Date("2023-06-02 06:03+01:00"),
+    startDate: new Date("2023-06-02T06:03+01:00"),
   },
   {
     type: EventType.TRIPURA_SUNDARI_PEAK,
-    startDate: new Date("2023-07-31 01:01+01:00"),
+    startDate: new Date("2023-07-31T01:01+01:00"),
   },
   {
     type: EventType.TRIPURA_SUNDARI_PEAK,
-    startDate: new Date("2023-08-29 08:24+01:00"),
+    startDate: new Date("2023-08-29T08:24+01:00"),
   },
   {
     type: EventType.TRIPURA_SUNDARI_PEAK,
-    startDate: new Date("2023-09-27 15:53+01:00"),
+    startDate: new Date("2023-09-27T15:53+01:00"),
   },
   {
     type: EventType.TRIPURA_SUNDARI_PEAK,
-    startDate: new Date("2023-10-27 00:25+01:00"),
+    startDate: new Date("2023-10-27T00:25+01:00"),
   },
   {
     type: EventType.TRIPURA_SUNDARI_PEAK,
-    startDate: new Date("2023-11-25 09:41+01:00"),
+    startDate: new Date("2023-11-25T09:41+01:00"),
   },
   {
     type: EventType.TRIPURA_SUNDARI_PEAK,
-    startDate: new Date("2023-12-24 22:03+01:00"),
+    startDate: new Date("2023-12-24T22:03+01:00"),
   },
 ];
 
 const fullMoon2023: EventBaseType[] = [
   {
     type: EventType.FULL_MOON_PEAK,
-    startDate: new Date("2023-01-07 00:08+01:00"),
+    startDate: new Date("2023-01-07T00:08+01:00"),
   },
   {
     type: EventType.FULL_MOON_PEAK,
-    startDate: new Date("2023-02-05 19:28+01:00"),
+    startDate: new Date("2023-02-05T19:28+01:00"),
   },
   {
     type: EventType.FULL_MOON_PEAK,
-    startDate: new Date("2023-03-07 13:40+01:00"),
+    startDate: new Date("2023-03-07T13:40+01:00"),
   },
   {
     type: EventType.FULL_MOON_PEAK,
-    startDate: new Date("2023-04-06 06:34+01:00"),
+    startDate: new Date("2023-04-06T06:34+01:00"),
   },
   {
     type: EventType.FULL_MOON_PEAK,
-    startDate: new Date("2023-05-05 19:34+01:00"),
+    startDate: new Date("2023-05-05T19:34+01:00"),
   },
   {
     type: EventType.FULL_MOON_PEAK,
-    startDate: new Date("2023-06-04 05:42+01:00"),
+    startDate: new Date("2023-06-04T05:42+01:00"),
   },
   {
     type: EventType.FULL_MOON_PEAK,
-    startDate: new Date("2023-07-03 13:39+01:00"),
+    startDate: new Date("2023-07-03T13:39+01:00"),
   },
   {
     type: EventType.FULL_MOON_PEAK,
-    startDate: new Date("2023-08-01 20:32+01:00"),
+    startDate: new Date("2023-08-01T20:32+01:00"),
   },
   {
     type: EventType.FULL_MOON_PEAK,
-    startDate: new Date("2023-08-31 03:36+01:00"),
+    startDate: new Date("2023-08-31T03:36+01:00"),
   },
   {
     type: EventType.FULL_MOON_PEAK,
-    startDate: new Date("2023-09-29 11:57+01:00"),
+    startDate: new Date("2023-09-29T11:57+01:00"),
   },
   {
     type: EventType.FULL_MOON_PEAK,
-    startDate: new Date("2023-10-28 22:24+01:00"),
+    startDate: new Date("2023-10-28T22:24+01:00"),
   },
   {
     type: EventType.FULL_MOON_PEAK,
-    startDate: new Date("2023-11-27 10:16+01:00"),
+    startDate: new Date("2023-11-27T10:16+01:00"),
   },
   {
     type: EventType.FULL_MOON_PEAK,
-    startDate: new Date("2023-12-27 01:33+01:00"),
+    startDate: new Date("2023-12-27T01:33+01:00"),
   },
 ];
 
 const shivaRatri2023: EventBaseType[] = [
   {
     type: EventType.SHIVARATRI,
-    startDate: new Date("2023-01-20 21:59+01:00"),
-    endDate: new Date("2023-01-21 00:55+01:00"),
+    startDate: new Date("2023-01-20T21:59+01:00"),
+    endDate: new Date("2023-01-21T00:55+01:00"),
   },
   {
     type: EventType.SHIVARATRI,
-    startDate: new Date("2023-02-18 22:09+01:00"),
-    endDate: new Date("2023-02-19 00:49+01:00"),
+    startDate: new Date("2023-02-18T22:09+01:00"),
+    endDate: new Date("2023-02-19T00:49+01:00"),
     description: "MSR",
   },
   {
     type: EventType.SHIVARATRI,
-    startDate: new Date("2023-03-20 22:13+01:00"),
-    endDate: new Date("2023-03-21 00:35+01:00"),
+    startDate: new Date("2023-03-20T22:13+01:00"),
+    endDate: new Date("2023-03-21T00:35+01:00"),
   },
   {
     type: EventType.SHIVARATRI,
-    startDate: new Date("2023-04-17 23:26+01:00"),
-    endDate: new Date("2023-04-19 01:17+01:00"),
+    startDate: new Date("2023-04-17T23:26+01:00"),
+    endDate: new Date("2023-04-19T01:17+01:00"),
   },
   {
     type: EventType.SHIVARATRI,
-    startDate: new Date("2023-05-16 23:17+01:00"),
-    endDate: new Date("2023-05-18 01:07+01:00"),
+    startDate: new Date("2023-05-16T23:17+01:00"),
+    endDate: new Date("2023-05-18T01:07+01:00"),
   },
   {
     type: EventType.SHIVARATRI,
-    startDate: new Date("2023-06-15 23:26+01:00"),
-    endDate: new Date("2023-06-17 01:07+01:00"),
+    startDate: new Date("2023-06-15T23:26+01:00"),
+    endDate: new Date("2023-06-17T01:07+01:00"),
   },
   {
     type: EventType.SHIVARATRI,
-    startDate: new Date("2023-07-14 23:29+01:00"),
-    endDate: new Date("2023-07-16 01:15+01:00"),
+    startDate: new Date("2023-07-14T23:29+01:00"),
+    endDate: new Date("2023-07-16T01:15+01:00"),
   },
   {
     type: EventType.SHIVARATRI,
-    startDate: new Date("2023-08-13 23:21+01:00"),
-    endDate: new Date("2023-08-15 01:20+01:00"),
+    startDate: new Date("2023-08-13T23:21+01:00"),
+    endDate: new Date("2023-08-15T01:20+01:00"),
   },
   {
     type: EventType.SHIVARATRI,
-    startDate: new Date("2023-09-11 23:04+01:00"),
-    endDate: new Date("2023-09-13 01:20+01:00"),
+    startDate: new Date("2023-09-11T23:04+01:00"),
+    endDate: new Date("2023-09-13T01:20+01:00"),
   },
   {
     type: EventType.SHIVARATRI,
-    startDate: new Date("2023-10-12 22:45+01:00"),
-    endDate: new Date("2023-10-13 01:19+01:00"),
+    startDate: new Date("2023-10-12T22:45+01:00"),
+    endDate: new Date("2023-10-13T01:19+01:00"),
   },
   {
     type: EventType.SHIVARATRI,
-    startDate: new Date("2023-11-11 21:35+01:00"),
-    endDate: new Date("2023-11-12 00:25+01:00"),
+    startDate: new Date("2023-11-11T21:35+01:00"),
+    endDate: new Date("2023-11-12T00:25+01:00"),
   },
   {
     type: EventType.SHIVARATRI,
-    startDate: new Date("2023-12-11 21:39+01:00"),
-    endDate: new Date("2023-12-12 00:39+01:00"),
+    startDate: new Date("2023-12-11T21:39+01:00"),
+    endDate: new Date("2023-12-12T00:39+01:00"),
   },
 ];
 const newMoon2023: EventBaseType[] = [
   {
     type: EventType.NEW_MOON,
-    startDate: new Date("2023-01-21 21:52+01:00"),
+    startDate: new Date("2023-01-21T21:52+01:00"),
   },
   {
     type: EventType.NEW_MOON,
-    startDate: new Date("2023-02-20 08:06+01:00"),
+    startDate: new Date("2023-02-20T08:06+01:00"),
   },
   {
     type: EventType.NEW_MOON,
-    startDate: new Date("2023-03-21 18:23+01:00"),
+    startDate: new Date("2023-03-21T18:23+01:00"),
   },
   {
     type: EventType.NEW_MOON,
-    startDate: new Date("2023-04-20 06:12+01:00"),
+    startDate: new Date("2023-04-20T06:12+01:00"),
   },
   {
     type: EventType.NEW_MOON,
-    startDate: new Date("2023-05-19 17:53+01:00"),
+    startDate: new Date("2023-05-19T17:53+01:00"),
   },
   {
     type: EventType.NEW_MOON,
-    startDate: new Date("2023-06-18 06:37+01:00"),
+    startDate: new Date("2023-06-18T06:37+01:00"),
   },
   {
     type: EventType.NEW_MOON,
-    startDate: new Date("2023-07-17 20:32+01:00"),
+    startDate: new Date("2023-07-17T20:32+01:00"),
   },
   {
     type: EventType.NEW_MOON,
-    startDate: new Date("2023-08-16 11:39+01:00"),
+    startDate: new Date("2023-08-16T11:39+01:00"),
   },
   {
     type: EventType.NEW_MOON,
-    startDate: new Date("2023-09-15 03:40+01:00"),
+    startDate: new Date("2023-09-15T03:40+01:00"),
   },
   {
     type: EventType.NEW_MOON,
-    startDate: new Date("2023-10-14 19:55+01:00"),
+    startDate: new Date("2023-10-14T19:55+01:00"),
   },
   {
     type: EventType.NEW_MOON,
-    startDate: new Date("2023-11-13 10:27+01:00"),
+    startDate: new Date("2023-11-13T10:27+01:00"),
   },
   {
     type: EventType.NEW_MOON,
-    startDate: new Date("2023-12-13 00:32+01:00"),
+    startDate: new Date("2023-12-13T00:32+01:00"),
   },
 ];
 const moonEclipse2023: EventBaseType[] = [
   {
     type: EventType.MOON_ECLIPSE,
-    startDate: new Date("2023-05-05 19:23+01:00"),
+    startDate: new Date("2023-05-05T19:23+01:00"),
     description: "Shadow",
   },
   {
     type: EventType.MOON_ECLIPSE,
-    startDate: new Date("2023-10-28 22:14+01:00"),
+    startDate: new Date("2023-10-28T22:14+01:00"),
     description: "Shadow",
   },
 ];
 const solarEclipse2023: EventBaseType[] = [
   {
     type: EventType.SOLAR_ECLIPSE,
-    startDate: new Date("2023-04-20 06:17+01:00"),
+    startDate: new Date("2023-04-20T06:17+01:00"),
     description: "Full",
   },
   {
     type: EventType.SOLAR_ECLIPSE,
-    startDate: new Date("2023-10-14 20:00+01:00"),
+    startDate: new Date("2023-10-14T20:00+01:00"),
     description: "Partial",
   },
 ];
