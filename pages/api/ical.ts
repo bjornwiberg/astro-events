293a61bde2f869b00788b46c42b8bbb9a474cfe4
Björn Wiberg <b@bjrn.nu>
2024-04-05 08:53:25 +0200

    wip

--- pages/api/ical.ts
+++ pages/api/ical.ts
@@ -20,7 +20,7 @@ function getTimestamp(dateString: string) {
     {
       locale: sv,
     }
-  )}00Z`;
+  )}00`;
 }
 
 export default function handler(
@@ -34,59 +34,94 @@ export default function handler(
       const sortedEvents = eventsData.sort((a, b) =>
         a.startDate > b.startDate ? 1 : -1
       );
-      res.status(200).send(`BEGIN:VCALENDAR
-PRODID:-//Björn Wiberg//astro events
-X-WR-CALNAME:Astrological events
-VERSION:2.0
-${sortedEvents
-  .reduce((events, event) => {
-    const { type, peakDate, startDate, endDate, description, updatedAt } =
-      event;
-    let { name } = getIconAndNameFromType(event.type);
+      const rows: string[] = [];
 
-    const eventDescription = description ? `\nDESCRIPTION:${description}` : "";
+      rows.push("BEGIN:VCALENDAR");
+      rows.push("PRODID:-//Björn Wiberg//astro events");
+      rows.push("X-WR-CALNAME:Astrological events");
+      rows.push("VERSION:2.0");
+      rows.push("BEGIN:VTIMEZONE");
+      rows.push("TZID:Europe/Stockholm");
+      rows.push("BEGIN:STANDARD");
+      rows.push("DTSTART;TZID=Europe/Stockholm:20220101T214900");
+      rows.push("TZOFFSETFROM:0000");
+      rows.push("TZOFFSETTO:+0100");
+      rows.push("TZNAME:EST");
+      rows.push("END:STANDARD");
 
-    const lastModified = updatedAt
-      ? `\nLAST-MODIFIED:${getTimestamp(updatedAt)}`
-      : "";
+      sortedEvents.forEach(
+        ({ type, peakDate, startDate, endDate, description, updatedAt }) => {
+          const descriptionAndLastModified: string[] = [];
+          const { name } = getIconAndNameFromType(type);
 
-    if (
-      type === EventType.TRIPURA_SUNDARI_PEAK ||
-      type === EventType.FULL_MOON_PEAK
-    ) {
-      const date = new Date(startDate);
-      const { start, end } =
-        type === EventType.TRIPURA_SUNDARI_PEAK
-          ? getTripuraSundariDatesFromPeakDate(date)
-          : getFullMoonDatesFromPeakDate(date);
+          if (description) {
+            descriptionAndLastModified.push(`DESCRIPTION:${description}`);
+          }
+          if (updatedAt) {
+            descriptionAndLastModified.push(
+              `LAST-MODIFIED:${getTimestamp(updatedAt)}`
+            );
+          }
 
-      events.push(`BEGIN:VEVENT
-SUMMARY:${name}${eventDescription}${lastModified}
-DTSTART;TZID=Europe/Stockholm:${getTimestamp(start.toISOString())}
-DTEND;TZID=Europe/Stockholm:${getTimestamp(end.toISOString())}
-END:VEVENT`);
+          if (type === EventType.TRIPURA_SUNDARI_PEAK) {
+            const date = new Date(startDate);
+            const { start, end } = getTripuraSundariDatesFromPeakDate(date);
 
-      name += " peak";
-    }
+            rows.push("BEGIN:VEVENT");
+            rows.push(`SUMMARY:${name} peak`);
+            rows.concat(descriptionAndLastModified);
+            rows.push(
+              `DTSTART;TZID=Europe/Stockholm:${getTimestamp(
+                start.toISOString()
+              )}`
+            );
+            rows.push(
+              `DTEND;TZID=Europe/Stockholm:${getTimestamp(end.toISOString())}`
+            );
+            rows.push("END:VEVENT");
+          }
 
-    if (peakDate) {
-      events.push(`BEGIN:VEVENT
-SUMMARY:${name} peak${eventDescription}${lastModified}
-DTSTART;TZID=Europe/Stockholm:${getTimestamp(peakDate)}
-DTEND;TZID=Europe/Stockholm:${getTimestamp(peakDate)}
-END:VEVENT`);
-    }
+          if (type === EventType.FULL_MOON_PEAK) {
+            const date = new Date(startDate);
+            const { start, end } = getFullMoonDatesFromPeakDate(date);
 
-    events.push(`BEGIN:VEVENT
-SUMMARY:${name}${eventDescription}${lastModified}
-DTSTART;TZID=Europe/Stockholm:${getTimestamp(event.startDate)}
-DTEND;TZID=Europe/Stockholm:${getTimestamp(endDate ?? event.startDate)}
-END:VEVENT`);
+            rows.push("BEGIN:VEVENT");
+            rows.push(`SUMMARY:${name} peak`);
+            rows.concat(descriptionAndLastModified);
+            rows.push(
+              `DTSTART;TZID=Europe/Stockholm:${getTimestamp(
+                start.toISOString()
+              )}`
+            );
+            rows.push(
+              `DTEND;TZID=Europe/Stockholm:${getTimestamp(end.toISOString())}`
+            );
+            rows.push("END:VEVENT");
+          }
 
-    return events;
-  }, [])
-  .join("\n")}
-END:VCALENDAR
-`);
+          if (peakDate) {
+            rows.push("BEGIN:VEVENT");
+            rows.push(`SUMMARY:${name} peak`);
+            rows.concat(descriptionAndLastModified);
+            rows.push(
+              `DTSTART;TZID=Europe/Stockholm:${getTimestamp(peakDate)}`
+            );
+            rows.push("END:VEVENT");
+          }
+
+          rows.push("BEGIN:VEVENT");
+          rows.push(`SUMMARY:${name}`);
+          rows.concat(descriptionAndLastModified);
+          rows.push(`DTSTART;TZID=Europe/Stockholm:${getTimestamp(startDate)}`);
+          if (endDate)
+            rows.push(`DTSTART;TZID=Europe/Stockholm:${getTimestamp(endDate)}`);
+          rows.push("END:VEVENT");
+        }
+      );
+
+      rows.push("END:VTIMEZONE");
+      rows.push("END:VCALENDAR");
+
+      res.status(200).send(rows.join("\n"));
   }
 }
