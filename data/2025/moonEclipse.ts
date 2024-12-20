f0c887c93c363a41a127580db77dc761e82336d1
Björn Wiberg <b@bjrn.nu>
2024-12-20 15:28:06 +0100

    fix: adjust dates for eclipses

--- data/2025/moonEclipse.ts
+++ data/2025/moonEclipse.ts
@@ -3,12 +3,12 @@ import { EventBaseType, EventType } from "../../types/events";
 const moonEclipse: EventBaseType[] = [
   {
     type: EventType.MOON_ECLIPSE,
-    startDate: "2025-03-14T07:55+01:00",
+    startDate: "2025-03-14T06:25+01:00",
     description: "Partial",
   },
   {
     type: EventType.MOON_ECLIPSE,
-    startDate: "2025-09-07T20:09+01:00",
+    startDate: "2025-09-07T20:11+01:00",
     description: "Full",
   },
 ];
--- data/2025/solarEclipse.ts
+++ data/2025/solarEclipse.ts
@@ -3,12 +3,8 @@ import { EventBaseType, EventType } from "../../types/events";
 const solarEclipse: EventBaseType[] = [
   {
     type: EventType.SOLAR_ECLIPSE,
-    startDate: "2025-03-29T11:58+01:00",
+    startDate: "2025-03-29T12:24+01:00",
     description: "Partial",
-  },
-  {
-    type: EventType.SOLAR_ECLIPSE,
-    startDate: "2025-09-21T21:54+01:00",
   },
 ];
 
