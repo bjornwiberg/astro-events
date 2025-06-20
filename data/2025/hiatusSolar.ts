8d1c8ebef6ad4558e7708306005be57e6132e312
Björn Wiberg <b@bjrn.nu>
2025-06-20 21:31:00 +0200

    chore: add solar hiatus 2025 (#425)

--- data/2025/hiatusSolar.ts
+++ data/2025/hiatusSolar.ts
@@ -1,10 +1,10 @@
 import { EventBaseType, EventType } from "../../types/events";
 
 const hiatusSolar: EventBaseType[] = [
-  // {
-  //   type: EventType.HIATUS_SOLAR,
-  //   startDate: "2025-08-27T03:35:00+01:00",
-  // },
+  {
+    type: EventType.HIATUS_SOLAR,
+    startDate: "2025-08-27T09:10:00+01:00",
+  },
 ];
 
 export default hiatusSolar;
