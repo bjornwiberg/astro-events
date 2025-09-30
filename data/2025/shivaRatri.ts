53b27b6c049edee0f472619d1c89f0b70547f163
Björn Wiberg <b@bjrn.nu>
2025-09-30 14:42:51 +0200

    fix: shivaratri september 2025

--- data/2025/shivaRatri.ts
+++ data/2025/shivaRatri.ts
@@ -44,8 +44,8 @@ const shivaRatri: EventBaseType[] = [
   },
   {
     type: EventType.SHIVARATRI,
-    startDate: "2025-09-19T22:54+01:00",
-    endDate: "2025-09-20T01:13+01:00",
+    startDate: "2025-09-19T23:54+01:00",
+    endDate: "2025-09-20T02:13+01:00",
   },
   {
     type: EventType.SHIVARATRI,
