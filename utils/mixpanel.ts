0cb1fc8cb64e6135dd320a6dc32d0a9a28c4d915
Björn Wiberg <b@bjrn.nu>
2025-12-16 15:52:25 +0100

    chore: update TypeScript configuration and remove unused Mixpanel types

--- utils/mixpanel.ts
+++ utils/mixpanel.ts
@@ -3,11 +3,14 @@ import mixpanel from "mixpanel-browser";
 
 export function initMixpanel() {
   if (process.env.NODE_ENV === "production") {
-    mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_TOKEN, {
-      ignore_dnt: true,
-    });
-    mixpanel.register({ Environment: process.env.NODE_ENV });
-    track("Enter page");
+    const token = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN;
+    if (token) {
+      mixpanel.init(token, {
+        ignore_dnt: true,
+      });
+      mixpanel.register({ Environment: process.env.NODE_ENV });
+      track("Enter page");
+    }
   }
 }
 
