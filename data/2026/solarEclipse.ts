c1bab051930029323ec0ffbaa1a00c524d80f0ac
Björn Wiberg <b@bjrn.nu>
2025-12-16 16:17:17 +0100

    fix: update solar eclipse description from "Total" to "Full"

--- data/2026/solarEclipse.ts
+++ data/2026/solarEclipse.ts
@@ -9,7 +9,7 @@ const solarEclipse: EventBaseType[] = [
   {
     type: EventType.SOLAR_ECLIPSE,
     startDate: "2026-08-12T20:46:00+02:00",
-    description: "Total",
+    description: "Full",
   },
 ];
 
