b4b8269d0cd0481a8c796be20daa0fd69f868718
Björn Wiberg <b@bjrn.nu>
2025-12-16 15:23:44 +0100

    fix: ensure Mixpanel initialization and tracking only occur in production with a valid token

--- utils/mixpanel.ts
+++ utils/mixpanel.ts
@@ -1,17 +1,20 @@
 import mixpanel from "mixpanel-browser";
 
 export function initMixpanel() {
-  if (process.env.NODE_ENV === "production") {
-    mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_TOKEN, {
-      ignore_dnt: true,
-    });
-    mixpanel.register({ Environment: process.env.NODE_ENV });
-    track("Enter page");
-  }
+	if (
+		process.env.NODE_ENV === "production" &&
+		process.env.NEXT_PUBLIC_MIXPANEL_TOKEN
+	) {
+		mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_TOKEN, {
+			ignore_dnt: true,
+		});
+		mixpanel.register({ Environment: process.env.NODE_ENV });
+		track("Enter page");
+	}
 }
 
 export function track(message: string, options?: Record<string, any> | string) {
-  if (process.env.NODE_ENV === "production") {
-    mixpanel.track(message, options);
-  }
+	if (process.env.NODE_ENV === "production") {
+		mixpanel.track(message, options);
+	}
 }
